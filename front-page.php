66fe95e7e44711a1735259ed91dafe907f006e3a
NERI <piero.neri@edu.univ-fcomte.fr>
2021-01-19 11:26:12 +0100

    Ajout front-page.php pour l'accueil

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php 
+    /** @link https://developer.wordpress.org/reference/functions/get_header/ */
+    get_header();
+
+    /** @link https://developer.wordpress.org/themes/basics/the-loop/  */
+    if( have_posts() ): 
+
+        while( have_posts() ) : the_post();
+
+        endwhile; 
+        
+    else:
+        
+    endif;
+
+    /** @link https://developer.wordpress.org/reference/functions/get_footer/ */
+    get_footer();
+?>
