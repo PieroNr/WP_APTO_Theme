b39d5b2611ccfef48094e1ad5d49c1ab63e8debe
NERI <piero.neri@edu.univ-fcomte.fr>
2021-01-19 12:08:17 +0100

    Ajout custom-features.php pour caractéristiques

---
+++ custom-features.php
@@ -0,0 +1,22 @@
+<?php 
+    /*Template Name: features */
+    /** @link https://developer.wordpress.org/themes/template-files-section/page-template-files/#creating-custom-page-templates-for-global-use  */
+?>
+
+<?php 
+    /** @link https://developer.wordpress.org/reference/functions/get_header/ */
+    get_header();
+
+    /** @link https://developer.wordpress.org/themes/basics/the-loop/  */
+    if( have_posts() ): while( have_posts() ) : the_post();
+
+
+        /** @link https://developer.wordpress.org/reference/functions/get_template_part/ */
+        get_template_part( 'template-parts/content', get_post_type() );
+    endwhile; else:
+        
+    endif;
+
+    /** @link https://developer.wordpress.org/reference/functions/get_footer/ */
+    get_footer();
+?>
