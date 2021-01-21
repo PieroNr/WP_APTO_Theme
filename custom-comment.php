24a2f3151ee0de5785bc6ccd8e37dbeff1bd8059
Tom JACOB <tom.jacob@outlook.fr>
2021-01-21 13:54:39 +0100

    [ADD] Moyenne des notes des caracteristiques

--- custom-comment.php
+++ custom-comment.php
@@ -11,6 +11,11 @@
     $numberNoteTwo = 0;
     $numberNoteOne = 0;
 
+    $totalConfort = 0;
+    $totalDesign = 0;
+    $totalBatterie = 0;
+    $totalPrix = 0;
+
 
     $pagetitle = get_field('page_title');
 
@@ -20,11 +25,20 @@
         while( have_rows('commentaire') ): the_row();
         $auteur = get_sub_field('avis_auteur');
 
-        echo 'Maxi pute';
-
         echo '<pre>';
         print_r(get_row());
         echo '</pre>';
+        $noteCarac = get_sub_field('note_caracteristique');
+
+        $totalConfort += $noteCarac['carac_confort']['carac_confort_note'];
+        $totalDesign += $noteCarac['carac_design']['carac_design_note'];
+        $totalBatterie += $noteCarac['carac_batterie']['carac_batterie_note'];
+        $totalPrix += $noteCarac['carac_prix']['carac_prix_note'];
+
+        echo '<pre>';
+        print_r($test);
+        echo '</pre>';
+
         switch (get_sub_field('avis_note')) {
             case 1:
                 $numberNoteOne++;
@@ -65,6 +79,12 @@
     <h1>Note 3/5 : <?php echo $numberNoteThree ?> - <?php echo round((($numberNoteThree / count($note))*100)); ?>% </h1>
     <h1>Note 4/5 : <?php echo $numberNoteFour ?> - <?php echo round((($numberNoteFour / count($note))*100)); ?>% </h1>
     <h1>Note 5/5 : <?php echo $numberNoteFive ?> - <?php echo round((($numberNoteFive / count($note))*100)); ?>% </h1>
+
+    <h2> Moyenne des notes de caractéristiques : </h2>
+    <h3> Confort : <?php echo round($totalConfort / $nbrNotes); ?>/5 </h3>
+    <h3> Design : <?php echo round($totalDesign / $nbrNotes); ?>/5 </h3>
+    <h3> Batterie : <?php echo round($totalBatterie / $nbrNotes); ?>/5 </h3>
+    <h3> Prix : <?php echo round($totalPrix / $nbrNotes); ?>/5 </h3>
 
     <?php
 
