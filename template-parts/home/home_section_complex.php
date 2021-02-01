bbc9e29f65e60b39802a0cbe06fe1b9926134201
NERI <piero.neri@edu.univ-fcomte.fr>
2021-02-01 22:25:57 +0100

    Ajout section complexe style

--- template-parts/home/home_section_complex.php
+++ template-parts/home/home_section_complex.php
@@ -9,7 +9,7 @@
                  <div class="complex-content">
                     <h1 class="complex-content__title"><?php echo $section_title; ?></h1>
 
-                    <div class="complex-content-options">                 
+                    <div class="complex-content-options row">                 
 
                         <?php
                         $section_options = get_sub_field('home_section_complex_fonction');
@@ -19,19 +19,22 @@
                             $options_description =$section_options[$i]['home_section_complex_fonction_description'];
                             $options_picto =$section_options[$i]['home_section_complex_fonction_pictogram'];  ?>  
 
-                            <div class="complex-content-options-one">
-                                <div class="complex-content-options-one-picto"><?php echo wp_get_attachment_image($options_picto['ID']); ?></div>
-                                <p class="complex-content-options-one__title"><?php echo $options_title; ?></p>
-                                <p class="complex-content-options-one__desc"><?php echo $options_description; ?></p>
+                            <div class="complex-content-options-one col-xl-6 col-l-6 col-md-12">
+                                <div class="complex-content-options-one-picto"><?php echo wp_get_attachment_image($options_picto['ID'],array(125,125)); ?></div>
+                                <div class="complex-content-options-one-titleDesc">
+                                    <p class="complex-content-options-one-titleDesc__title"><?php echo $options_title; ?></p>
+                                    <p class="complex-content-options-one-titleDesc__desc"><?php echo $options_description; ?></p>
+                                </div>
+                                
                             </div>
                         <?php
                         } ?>
 
                     </div>
 
-                <?php $section_image= wp_get_attachment_image( get_sub_field('home_section_complex_image')['ID']);  ?>        
+                <?php $section_image= wp_get_attachment_image_src( get_sub_field('home_section_complex_image')['ID'],'full');  ?>        
                         
-                        <div class="complex-content-image"><?php echo $section_image; ?></div>
+                        <div class="complex-content-image" style="background-image: url(<?php echo $section_image[0]; ?>);"></div>
             
                 </div>
             </div>
